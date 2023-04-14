601da25e85634e194cfd8dcfde158627a1d06c8f
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-14 17:13:56 +0600

    init

---
+++ ViewPresenter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ViewPresenter
+//
+//  Created by Shah Md Imran Hossain on 14/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewPresenter/ViewPresenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewPresenterApp.swift
+//  ViewPresenter
+//
+//  Created by Shah Md Imran Hossain on 14/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewPresenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
