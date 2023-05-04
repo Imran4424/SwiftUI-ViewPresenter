b29a15779a34e37d4ec916ee4a3a7ebc268db74b
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-05-04 21:25:39 +0600

    modal view full screen

--- ViewPresenter/ContentView.swift
+++ ViewPresenter/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 // sheet is alernative to showing views modally in UIKit
-struct SecondView: View {
+struct ModalView: View {
     @Environment(\.dismiss) var dismiss
     
     let name: String
@@ -23,16 +23,99 @@ struct SecondView: View {
     }
 }
 
+struct Person: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+struct PersonListView: View {
+    let people = [
+        Person(name: "Soudha"),
+        Person(name: "Arongka"),
+        Person(name: "Imran"),
+        Person(name: "Anik"),
+        Person(name: "Nayeem"),
+        Person(name: "Linkon"),
+        Person(name: "Munir"),
+        Person(name: "Rakib"),
+        Person(name: "Horidas(Harry)")
+    ]
+    
+    @Environment(\.dismiss) var dismiss
+    @State private var selectedPerson: Person?
+
+    var body: some View {
+        VStack {
+            List(people) { person in
+                Button(action: {
+                    selectedPerson = person
+                }) {
+                    Text(person.name)
+                }
+            }
+            
+            Spacer()
+            
+            Button("Dismiss") {
+                dismiss()
+            }
+        }
+        .fullScreenCover(item: $selectedPerson) { person in
+            PersonDetailView(person: person)
+        }
+    }
+}
+
+struct PersonDetailView: View {
+    @Environment(\.dismiss) var dismiss
+    let person: Person
+
+    var body: some View {
+        VStack {
+            Spacer()
+            Text(person.name)
+                .font(.title)
+            
+            Spacer()
+            
+            Button("Dismiss") {
+                dismiss()
+            }
+        }
+        .padding()
+    }
+}
+
 struct ContentView: View {
     @State private var showingSheet = false
+    @State private var showingFullScreen = false
+    @State private var showingPerson = false
     
     var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
+        VStack(spacing: 20) {
+            Button("Show Sheet") {
+                showingSheet.toggle()
+            }
+            .sheet(isPresented: $showingSheet) {
+                ModalView(name: "Imran")
+            }
+            
+            Button("Show in fullscreen") {
+                showingFullScreen.toggle()
+            }
+            .fullScreenCover(isPresented: $showingFullScreen) {
+                ModalView(name: "Imran in fullscreen")
+            }
+            
+            Button("Show in fullscreen Item") {
+                showingPerson.toggle()
+            }
+            .fullScreenCover(isPresented: $showingPerson) {
+                PersonListView()
+            }
+            
         }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "Imran")
-        }
+        
     }
 }
 
